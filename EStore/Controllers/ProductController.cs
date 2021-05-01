205872ba8fab6fb70699f30bb6ecbd4fb13770e0
vaidehi <vaidehihirani@gmail.com>
2021-05-01 17:50:10 +0530

    Initial commit

---
+++ EStore/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EStore.Models;
+
+namespace EStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
---
+++ EStore/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EStore.DbContext
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
